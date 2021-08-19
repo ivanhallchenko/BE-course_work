6041acb9f9c734198b7e21472540e1264fbd6826
ivanhallchenko <ivan.hallchenko@logiqlabs.com>
2021-08-19 18:38:33 +0300

    Add comments

--- src/specs/forTwoCubes.spec.js
+++ src/specs/forTwoCubes.spec.js
@@ -33,20 +33,19 @@ let throwPersentage = {
 
  describe('Throwing two dice 100, 1000, 5000 times', function() {
    it('Two dice rolls 100', async function () {
-     let resultOf100Rolls = await userHelper.get2Cubes100Rolls(agent);
-     allValues = resultOf100Rolls.text;
-     allValues = allValues.split("\n");
-     allValues.pop();
-     allValues = allValues.map( i => {
+     let resultOf100Rolls = await userHelper.get2Cubes100Rolls(agent); // GET response
+     allValues = resultOf100Rolls.text;                                // Find our values in body
+     allValues = allValues.split("\n");                                // Converting to one line
+     allValues.pop();                                                  // Cut off the last space
+     allValues = allValues.map( i => {                                 // Loop through the array, in which we remove tabs
        // '1\t4'
-       let elem = i.split("\t").map(j => parseInt(j));
+       let elem = i.split("\t").map(j => parseInt(j));                 // And convert the text to numbers for each element in new array
        // ["1", "4"]
-       //return parseInt(elem[0]) + parseInt(elem[1]);
-       return elem[0] + elem[1];
+       return elem[0] + elem[1];                                       // Then we return the sum of the two faces to the overwritten array
      });
      console.log("ALL DICE ROLLS TOTAL \n " + allValues + "\n");
 
-     for(let k = 0; k < allValues.length; k++){
+     for(let k = 0; k < allValues.length; k++){                        // Count the number of dice points dropped out the same time, from 2 to 12
        switch (allValues[k]){
          case 2:
            quantityOfValues.quantityOf2++;
@@ -85,15 +84,18 @@ let throwPersentage = {
      }
      console.log("The number of drops of the same amount \n" + Object.values(quantityOfValues));
 
+     // Consider the percentage of each amount dropped relative to the ideal (8.3 times per 100 throws)
      for (let k = 2; k < Object.keys(quantityOfValues).length+2; k++ ){
        throwPersentage[k] = (Math.round(100 * (quantityOfValues['quantityOf' + k] / 8.3)));
      }
-     console.log("The Fallout Percentage of each number, relative to the ideal.\n For 100 rolls, 100 percent distribution would be 8.3 for each total of two dice \n" + Object.values(throwPersentage));
-     // переписать фунцию для вывода проверки гипотезы на 100, 1000, 5000 бросков
+     console.log("The Fallout Percentage of each number, relative to the ideal.\nFor 100 rolls, 100 percent distribution would be 8.3 for each total of two dice \n" + Object.values(throwPersentage)+ "\n\n");
+
      for (let j = 2; j < 13; j++){
        console.log(`The amount ${j} dropped out ${Object.values(quantityOfValues['quantityOf' + j])} times instead of 8.3. The percentage of deviation was ${(100 - throwPersentage[j]) + "%"}`);
      }
 
+
+     // переписать фунцию для вывода проверки гипотезы на 100, 1000, 5000 бросков
 
    });
    // it('Two dice rolls 1000', async function () {
